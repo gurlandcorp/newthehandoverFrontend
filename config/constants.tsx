53f6a715e97d521fd45a5863838ebbc47d47012c
Xetech Soft <xetechsoft@gmail.com>
2022-02-26 00:17:18 +0500

    main

--- config/constants.tsx
+++ config/constants.tsx
@@ -1,3 +1,3 @@
 const API_LINK = 'https://handoverapi.herokuapp.com';
-const Base_URL = process.env.NODE_ENV=='development' ? 'http://127.0.0.1:3000' : 'http://127.0.0.1:3000';
+const Base_URL = process.env.NODE_ENV=='development' ? 'http://127.0.0.1:3000' : 'https://thehandover.com';
 export { API_LINK, Base_URL }
